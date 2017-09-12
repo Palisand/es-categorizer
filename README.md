96e1a3e4d93ac13e9f1ee2f8dc81817861d345ba
Palisand <p9alisand@gmail.com>
2017-09-11 20:10:11 -0400

    implement chunk loading; use docopt

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 # es-categorizer 
-Extracting categories from an arbitrary block of text using elasticsearch.
+Extracting categories from an arbitrary block of text using Elasticsearch and Wikipedia.
 
 ### How?
 
 Given some text, [term vectors](https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-termvectors.html) and the [OpenNLP Ingest Processor plugin](https://github.com/spinscale/elasticsearch-ingest-opennlp) can be used to extract keywords that exist **only** within that text, which is not suitable for category extraction. What is needed is a dataset that includes indexable content paired with tags, like an encyclopedia:
 
-1. Index Wikipedia. See [this guide](https://www.elastic.co/blog/loading-wikipedia).
-    1. Download the [latest dump of the search index](https://dumps.wikimedia.org/other/cirrussearch/current/enwiki-20170904-cirrussearch-content.json.gz).
-    2. 
+1. Index Wikipedia. See [this guide](https://www.elastic.co/blog/loading-wikipedia) and *Setup* below.
 2. Use a [More Like This Query](https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-mlt-query.html) to find documents (wiki articles) that match a given text.
 3. Return the categories of the highest scoring documents.
 
@@ -25,3 +23,9 @@ brew services start elasticsearch  # or just "elasticsearch" for foreground exec
 elasticsearch-plugin install analysis-icu
 elasticsearch-plugin install org.wikimedia.search:extra:5.5.2
 ```
+
+### Setup
+
+TODO: finish guide
+
+1. Download the [latest dump of the search index](https://dumps.wikimedia.org/other/cirrussearch/current/enwiki-20170904-cirrussearch-content.json.gz).
--- main.py
+++ main.py
@@ -1,23 +1,64 @@
 #!/usr/bin/env python
 """
-Extract categories from text using elasticsearch.
+Extract categories from text using Elasticsearch and Wikipedia.
 
 Usage:
+  main.py
   main.py create_index
+  main.py delete_index
+  main.py chunk <filepath>
+  main.py load [-d | --delete]
+  main.py (-h | --help)
+
+Options:
+ -h --help      Show this screen.
+ -d --delete    Delete chunk-files after loading them.
+
 """
 import os
+import sys
 import gzip
 import json
-# progressbar
-# from docopt import docopt
+import subprocess
+from docopt import docopt
 from elasticsearch import Elasticsearch
+# from elasticsearch.helpers import bulk
+try:
+    import progressbar
+    MOCK_PROGRESSBAR = False
+except ImportError:
+    MOCK_PROGRESSBAR = True
 
 es = Elasticsearch()
 INDEX_NAME = "enwiki"
 
 
-def create_index():
+class MockProgressBar(object):
+    """
+    Mock progressbar.ProgressBar
+    (Used if progressbar2 is not installed.)
+    """
 
+    def __init__(self, max_value):
+        self.max_value = max_value
+
+    def update(self, num):
+        print('{:.0f}% ({} of {})'.format(
+            (num / self.max_value) * 100, num, self.max_value))
+        print("\x1b[1A\x1b[2K", end='')
+
+    def finish(self):
+        print('100% ({0} of {0})'.format(self.max_value))
+
+
+def delete_index():
+    """ Delete the index if it exits. """
+    if es.indices.exists(INDEX_NAME):
+        es.indices.delete(index=INDEX_NAME)
+
+
+def create_index():
+    """ Create the index if it doesn't exist. """
     if not es.indices.exists(INDEX_NAME):
 
         def load_index_config(config):
@@ -67,9 +108,9 @@ def chunk_gzipped_file(filepath, prefix):
                 os.path.join(chunk_dir, "{}{:04d}".format(prefix, num_chunk)), "wb"
             ) as fp_chunk:
                 for line in fp:
-                    if num_line < line_chunksize:
+                    fp_chunk.write(line)
+                    if num_line < line_chunksize - 1:
                         num_line += 1
-                        fp_chunk.write(line)
                     else:
                         break
                 else:
@@ -80,26 +121,64 @@ def chunk_gzipped_file(filepath, prefix):
         print("chunks: {}".format(num_chunk + 1))
 
 
-def load_chunks():
-    pass
+def load_chunks(delete=False):
+
+    # FIXME: use bulk / parallel_bulk helper instead of subprocess if possible
+    # https://stackoverflow.com/questions/46164822/elasticsearch-py-bulk-helper-equivalent-of-curl-with-file
+    #
+    # with open("chunks/enwiki000x") as fp:
+    #     bulk(
+    #         client=es,
+    #         index=INDEX_NAME,
+    #         actions=fp
+    #     )
+
+    chunks = os.listdir("chunks")
+    bar = progressbar.ProgressBar if not MOCK_PROGRESSBAR else MockProgressBar
+    bar = bar(max_value=len(chunks))
+
+    num_failed_chunks = 0
+    for i, chunkname in enumerate(chunks):
+        therighteouspathofthechunk = os.path.join("chunks", chunkname)
+        try:
+            subprocess.check_output([
+                "curl",
+                "-s",
+                "-XPOST",
+                "localhost:9200/{}/_bulk".format(INDEX_NAME),
+                "--data-binary",
+                "@{}".format(therighteouspathofthechunk)
+            ])
+        except subprocess.CalledProcessError:
+            num_failed_chunks += 1
+            print("Failed to load {}".format(therighteouspathofthechunk))
+        else:
+            delete and os.remove(therighteouspathofthechunk)
+        bar.update(i + 1)
+    bar.finish()
+
+    if num_failed_chunks:
+        print("Failed to load {} chunk files.".format(num_failed_chunks))
 
 
 def get_categories_for_text(text):
-    es.search(
+    return es.search(
         index=INDEX_NAME,
         doc_type="page",
         body={
             "query": {
                 "more_like_this": {
                     "fields": [
-                        "content"
+                        "source_text.plain"
                     ],
                     "like": text,
-                    # "min_term_freq": 1,
-                    # "max_query_terms": 20,
+                    "min_term_freq": 1,
+                    "max_query_terms": 20,
+                    "min_doc_freq": 1
                 }
             }
         },
+        _source="title",
         # _source="categories"  # try list
         # filter_path=["hits.hits._source"]
     )
@@ -109,22 +188,29 @@ def setup():
     """
     Build it, chunk it, load it.
     """
+    delete_index()
     create_index()
     while True:
-        dump = input("path to search index dump: ")
+        dump = input("path to gzipped search index dump: ")
         if os.path.exists(dump):
             break
         print("File not found. Try again.")
     chunk_gzipped_file(dump, INDEX_NAME)
-    # load_chunks()
+    load_chunks()
 
 
-def main():
-    setup()
+def main(argv):
+    doc = docopt(__doc__)
 
-    # docopt
+    if len(argv) == 1:
+        setup()
+    else:
+        doc["create_index"] and create_index()
+        doc["delete_index"] and delete_index()
+        doc["chunk"] and chunk_gzipped_file(doc["<filepath>"], INDEX_NAME)
+        doc["load"] and load_chunks(doc["--delete"])
 
-    # chunk_wikipedia("/Users/palisand/Desktop/enwiki-20170904-cirrussearch-content.json.gz")
+    # print(get_categories_for_text("Tamas is a Field Marshal"))  # matches "The Crimson Campaign"
 
     # with open("fixtures.txt") as fp:
     #     for line in fp:
@@ -132,4 +218,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
